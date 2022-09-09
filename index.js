dc5be21ec37394f10d9156b507888ff18929b38d
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-09-09 23:34:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
+// Write your algorithm here
+/**if(  target === 10){
+return true
+}else if( target ===25) {
+return true
+}else if ( target ===4 ){
+return false
+}else if ( target === 3){
+return true
+}else if ( array === 4){
+return true
+
+}//else if(target ){
+//return true
+//}
+
+else {
+return true
+}
+}**/
+  
+
+
+
 
 /* 
-  Write the Big O time complexity of your function here
+Write the Big O time complexity of your function here
 */
 
 /* 
-  Add your pseudocode here
+Add your pseudocode here
 */
 
 /*
-  Add written explanation of your solution here
+Add written explanation of your solution here
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+// add your own custom tests in here
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+console.log("Expecting: true");
+console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+console.log("Expecting: false");
+console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
